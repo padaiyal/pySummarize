5cb349b4bdf5c7f4bf265970ee409da450b4387e
Ranjan Mohan <ranjanmj@gmail.com>
2019-06-15 09:31:01 -0500

    Initial commit
    Added v1.0 of the summarize script. Refer to the version_history
    folder for more details.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='summarize',
+    version='1.0',
+    packages=['src'],
+    url='',
+    license='',
+    author='Ranjan Mohan',
+    author_email='',
+    description='A commandline tool to summarize file data',
+    install_requires=[
+        'python-dateutil',
+        'matplotlib',
+        'colorama',
+        'pyqt5'
+    ]
+)

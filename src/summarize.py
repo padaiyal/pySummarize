e2a4e14b1913bd0eaae24278c1f6bcf25d09f773
Ranjan Mohan <ranjanmj@gmail.com>
2019-06-18 01:08:37 -0500

    Made --file_type optional
    This kwarg is now optional as the input requirements for the file_type CSV
    are already checked.

--- src/summarize.py
+++ src/summarize.py
@@ -6,7 +6,7 @@ import optparse
 
 parser = optparse.OptionParser(description='Summarize information')
 parser.add_option('--summary_type', type=str, help='Type of summary')
-parser.add_option('--file_type', type=str, help='Type of input file')
+parser.add_option('--file_type', type=str, help='Type of input file', default='unknown')
 parser.add_option('--graph_type', type=str, help='Type of graph')
 parser.add_option('--label_regex', type=str, help='Regex to search for')
 parser.add_option('--legend_regex', type=str, help='Regex to search for')
@@ -66,99 +66,97 @@ delimiter = options.delimiter
 normal_type = None
 y_data = dict()
 
-if file_type.upper() == "CSV":
-    print(f"CSV found! - {file_path}")
-    if summary_type.lower() == 'timeline':
-        normal_type = 'normal'
-        i = 0
-        previous_i = 0
-        occurrences = dict()
-        current_label = None
-        previous_label = None
-        legend_set = set()
-        with open(file_path, 'r') as csv_file:
-            for line in csv_file:
-                row = line.split(delimiter)
-                legend = row[legend_column_index]
-                if i == 0:
-                    previous_label = str(i) if label_column_index is None else row[label_column_index]
-                current_label = str(i) if label_column_index is None else row[label_column_index]
-                if legend not in occurrences \
-                        and (legend_regex is None or (file_type == "CSV" and re.match(legend_regex, legend))
-                             or (file_type != "CSV" and re.findall(legend_regex, legend))):
-                    occurrences[legend] = 0
-                if legend not in legend_set \
-                        and (legend_regex is None or (file_type == "CSV" and re.match(legend_regex, legend))
-                             or (file_type != "CSV" and re.findall(legend_regex, legend))):
-                    legend_set.add(legend)
-                    y_data[legend] = [0] * len(labels)
-                if legend in occurrences:
-                    occurrences[legend] += 1
-                if i - previous_i >= label_frequency:
-                    labels.append(current_label)
-                    term_data.append(list(occurrences.values()))
-                    term_legend.append(list(occurrences.keys()))
-                    for unique_legend in legend_set:
-                        y_datum = occurrences[unique_legend] if unique_legend in occurrences else 0
-                        y_data[unique_legend].append(y_datum)
-                    occurrences = dict()
-                    previous_label = current_label
-                    previous_i = i
-                i += 1
-                if rows_limit != 0 and i == rows_limit:
-                    break
-    elif summary_type.lower() == 'fd':
-        normal_type = 'bar'
-        frequency_distribution = dict()
-        i = 0
-        with open(file_path, 'r') as file:
-            for line in file:
-                if label_regex is not None:
-                    matches = re.findall(label_regex, line)
-                    for match in matches:
-                        if match not in frequency_distribution:
-                            frequency_distribution[match] = 0
-                        frequency_distribution[match] += 1
-                elif label_column_index is not None:
-                    label = line.split(delimiter)[label_column_index]
-                    if label not in frequency_distribution:
-                        frequency_distribution[label] = 0
-                    frequency_distribution[label] += 1
-                    pass
-                else:
-                    print('label_regex is None and either file is not a CSV or label_column_index is None! '
-                          'Please check.')
-                    exit(1)
-                i += 1
-                if rows_limit != 0 and i == rows_limit:
-                    break
-        frequency_distribution_sorted = sorted(frequency_distribution.items(), key=lambda x: x[1], reverse=True)
-        y_data = {label_regex: [item[1] for item in frequency_distribution_sorted]}
-        term_data = [[item[1]] for item in frequency_distribution_sorted]
-        labels = [item[0] for item in frequency_distribution_sorted]
-        term_legend = [['']] * len(term_data)
-
-    if graph_type.lower() == 'term':
-        termgraph.chart(supported_colors,
-                        term_data,
-                        args,
-                        labels,
-                        term_legend
-                        )
-    elif graph_type.lower() == 'normal':
-        for label, data in y_data.items():
-            if normal_type == 'bar':
-                fig, ax = plt.subplots()
-                for i, v in enumerate(data):
-                    ax.text(i - .4, v + 0.01 * v, str(v), color='blue', fontweight='bold')
-                plt.bar(labels, data, label=label)
+if summary_type.lower() == 'timeline':
+    normal_type = 'normal'
+    i = 0
+    previous_i = 0
+    occurrences = dict()
+    current_label = None
+    previous_label = None
+    legend_set = set()
+    with open(file_path, 'r') as csv_file:
+        for line in csv_file:
+            row = line.split(delimiter)
+            legend = row[legend_column_index]
+            if i == 0:
+                previous_label = str(i) if label_column_index is None else row[label_column_index]
+            current_label = str(i) if label_column_index is None else row[label_column_index]
+            if legend not in occurrences \
+                    and (legend_regex is None or (file_type == "CSV" and re.match(legend_regex, legend))
+                         or (file_type != "CSV" and re.findall(legend_regex, legend))):
+                occurrences[legend] = 0
+            if legend not in legend_set \
+                    and (legend_regex is None or (file_type == "CSV" and re.match(legend_regex, legend))
+                         or (file_type != "CSV" and re.findall(legend_regex, legend))):
+                legend_set.add(legend)
+                y_data[legend] = [0] * len(labels)
+            if legend in occurrences:
+                occurrences[legend] += 1
+            if i - previous_i >= label_frequency:
+                labels.append(current_label)
+                term_data.append(list(occurrences.values()))
+                term_legend.append(list(occurrences.keys()))
+                for unique_legend in legend_set:
+                    y_datum = occurrences[unique_legend] if unique_legend in occurrences else 0
+                    y_data[unique_legend].append(y_datum)
+                occurrences = dict()
+                previous_label = current_label
+                previous_i = i
+            i += 1
+            if rows_limit != 0 and i == rows_limit:
+                break
+elif summary_type.lower() == 'fd':
+    normal_type = 'bar'
+    frequency_distribution = dict()
+    i = 0
+    with open(file_path, 'r') as file:
+        for line in file:
+            if label_regex is not None:
+                matches = re.findall(label_regex, line)
+                for match in matches:
+                    if match not in frequency_distribution:
+                        frequency_distribution[match] = 0
+                    frequency_distribution[match] += 1
+            elif label_column_index is not None:
+                label = line.split(delimiter)[label_column_index]
+                if label not in frequency_distribution:
+                    frequency_distribution[label] = 0
+                frequency_distribution[label] += 1
+                pass
             else:
-                plt.plot(labels, data, label=label)
-            plt.xticks(labels, labels, rotation='vertical')
-        plt.legend(loc='upper center')
-        plt.title(file_path)
-        plt.tight_layout()
-        plt.show()
+                print('label_regex is None and either file is not a CSV or label_column_index is None! '
+                      'Please check.')
+                exit(1)
+            i += 1
+            if rows_limit != 0 and i == rows_limit:
+                break
+    frequency_distribution_sorted = sorted(frequency_distribution.items(), key=lambda x: x[1], reverse=True)
+    y_data = {label_regex: [item[1] for item in frequency_distribution_sorted]}
+    term_data = [[item[1]] for item in frequency_distribution_sorted]
+    labels = [item[0] for item in frequency_distribution_sorted]
+    term_legend = [['']] * len(term_data)
+
+if graph_type.lower() == 'term':
+    termgraph.chart(supported_colors,
+                    term_data,
+                    args,
+                    labels,
+                    term_legend
+                    )
+elif graph_type.lower() == 'normal':
+    for label, data in y_data.items():
+        if normal_type == 'bar':
+            fig, ax = plt.subplots()
+            for i, v in enumerate(data):
+                ax.text(i - .4, v + 0.01 * v, str(v), color='blue', fontweight='bold')
+            plt.bar(labels, data, label=label)
+        else:
+            plt.plot(labels, data, label=label)
+        plt.xticks(labels, labels, rotation='vertical')
+    plt.legend(loc='upper center')
+    plt.title(file_path)
+    plt.tight_layout()
+    plt.show()
 
 else:
-    print(f"Unsupported file type {file_type}")
+    print(f"Unsupported graph type {file_type}")
